b3f4a946fd2d28265b5db86283527fe14be584bd
ignaciosein@gmail.com <ignaciosein@gmail.com>
2021-07-22 00:10:15 +0200

    Se pintan markers en la pantalla

--- client/src/pages/Home/Home.js
+++ client/src/pages/Home/Home.js
@@ -1,26 +1,104 @@
 import * as React from "react";
 import { useState } from "react";
-import ReactMapGL from "react-map-gl";
-
+import ReactMapGL,{Marker , FlyToInterpolator} from "react-map-gl";
+import axios from "axios"
+import MarkerImg from "../../img/marker.png"
 import "./Home.scss";
 
 const Home = () => {
-  const [viewport, setViewport] = useState({
-    width: 400,
-    height: 400,
-    latitude: 37.7577,
-    longitude: -122.4376,
-    zoom: 8,
-  });
 
+
+const [wc , setWc] = useState([])
+const searchWc = async (e) =>{
+
+  e.preventDefault();
+
+ 
+
+  let wc = e.target.wc.value
+
+ 
+let obj = {
+
+  name: wc
+
+}
+  let resultWc = await axios.post("/api/search",obj );
+
+
+  console.log(resultWc.data)
+
+  setWc(resultWc.data)
+  
+
+}
+
+
+
+
+
+
+  const [viewport, setViewport] = useState({
+    width: "80vw",
+    height: "40vh",
+    latitude: 40.4205026,
+    longitude: -3.7254743,
+    zoom: 10,
+  });
+  console.log(wc)
+
+  const mapeo = () =>{
+
+
+
+
+    
+  }
+ 
   return (
+
+    <div>
+      <form onSubmit={searchWc}>
+      <label>Buscar WC</label>
+      <input type="text" name="wc"/>
+      <button >BUSCAR</button>
+      </form>
     <section className="Home">
-      <ReactMapGL
-        mapboxApiAccessToken={process.env.REACT_APP_MAPBOX}
-        {...viewport}
-        onViewportChange={(nextViewport) => setViewport(nextViewport)}
-      />
+      <ReactMapGL  mapboxApiAccessToken={process.env.REACT_APP_MAPBOX}  {...viewport}   onViewportChange={(nextViewport) => setViewport(nextViewport)}>
+   
+{wc.map((item,i)=>(  
+  
+  <Marker key={i}
+  latitude={item.latitude} 
+  longitude={item.longitude}
+  offsetLeft={-20}
+  offsetTop={-30}
+
+  
+ >
+<div className="marker" > 
+</div>
+
+ </Marker>       ))}
+
+{/*  <Marker
+  latitude={40.4153275} 
+  longitude={-3.7087922}
+  offsetLeft={-20}
+  offsetTop={-30}
+
+  
+ >
+<div><img className="marker" src="https://www.pngfind.com/pngs/m/326-3268483_location-logo-png-map-marker-alt-transparent-png.png"></img>
+</div>
+
+ </Marker> */}
+
+       </ReactMapGL>
     </section>
+    
+   
+    </div>
   );
 };
 
