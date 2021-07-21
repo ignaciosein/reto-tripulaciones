9224acdc258428e494370fc6643c2fd5dc0243b0
Adrian E. de Zamacona Garza <adriandez@gmail.com>
2021-07-21 09:30:31 +0200

    added basic map

--- client/src/pages/Home/Home.js
+++ client/src/pages/Home/Home.js
@@ -1,14 +1,25 @@
+import * as React from "react";
+import { useState } from "react";
+import ReactMapGL from "react-map-gl";
+
 import "./Home.scss";
 
 const Home = () => {
+  const [viewport, setViewport] = useState({
+    width: 400,
+    height: 400,
+    latitude: 37.7577,
+    longitude: -122.4376,
+    zoom: 8,
+  });
+
   return (
     <section className="Home">
-        <h3>Objetivo del del reto</h3>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur, tenetur odio, saepe doloremque atque veniam, fuga nostrum dolores eligendi alias eos natus voluptatibus. Numquam officiis itaque totam consequuntur, voluptates laudantium!</p>
-      <article>
-        <h4>Descripcion</h4>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi quidem aliquid, temporibus molestiae enim quam impedit quas distinctio pariatur dolorum repudiandae ullam est nam iusto nulla vero dolorem, illo odio?</p>
-      </article>
+      <ReactMapGL
+        mapboxApiAccessToken={process.env.REACT_APP_MAPBOX}
+        {...viewport}
+        onViewportChange={(nextViewport) => setViewport(nextViewport)}
+      />
     </section>
   );
 };
